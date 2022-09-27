b672f1068c8ea82755bf10f473dbd01f194bf298
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-09-27 14:23:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// text file
+let textButton = document.querrySelector('#text-btn');
+textButton.addEventlistener('click',function(){
+
+    // create an ajax request
+    let xhr = new XMLHttpRequest();
+
+    // prepare the request
+    xhr.open('GET','message.text',true);
+
+    // prepare the request
+    xhr.send();
+
+    // process the request
+    xhr.onload  = () =>{
+        if(xhr.status ===200){
+            let data = xhr.reposnseText;
+            displayTextData(data);
+        }
+    }
+});
+
+// display textdata
+let displayTextData = (data) =>{
+    let htmlTemplate = `<h3>${data}</h3>`;
+    document.querrySelector('#text-card').innerHTML = htmlTemplate;
+}
+
