859f3946bbac50106739f5b6ee34b14f43b012bc
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-01 17:41:54 +0300

    adding api data logic

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // text file
 let textButton = document.querrySelector('#text-btn');
-textButton.addEventlistener('click',function(){
+textButton.addEventlistener('click', function () {
 
     // create an ajax request
     let xhr = new XMLHttpRequest();
 
     // prepare the request
-    xhr.open('GET','message.text',true);
+    xhr.open('GET', 'message.text', true);
 
     // prepare the request
     xhr.send();
 
     // process the request
-    xhr.onload  = () =>{
-        if(xhr.status ===200){
+    xhr.onload = () => {
+        if (xhr.status === 200) {
             let data = xhr.reposnseText;
             displayTextData(data);
         }
@@ -21,8 +21,69 @@ textButton.addEventlistener('click',function(){
 });
 
 // display textdata
-let displayTextData = (data) =>{
+let displayTextData = (data) => {
     let htmlTemplate = `<h3>${data}</h3>`;
     document.querrySelector('#text-card').innerHTML = htmlTemplate;
+}
+
+// json button
+let jsonButton = document.querySelector("json-btn");
+jsonButton.addEventListener('click', function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', './mobile.json', true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = xhr.responseText;
+            let mobile = Json.parser(data);
+            console.log(mobile);
+        }
+    }
+});
+
+// display json dta
+let displayJSONData = (mobile) => {
+    let htmlTemplate = '';
+    htmlTemplate = `<ul>
+<li>ID :ID ${mobile.id}</li>
+<li>ID :ID ${mobile.brand}</li>
+<li>ID :ID ${mobile.color}</li>
+<li>ID :ID ${mobile.price}</li>
+</ul>;`
+
+    document.wuerySelector('#json-card').innerHTML = htmlTemplate
+};
+
+// api button
+
+let apiButton = document.querySelector('#api-btn');
+apiButton.addEventListener('click', function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = xhr.responseText;
+            let users = JSON.parse(data);
+            console.log(users);
+        }
+    }
+});
+
+// display users
+let displayUsers = (users) => {
+    letTemplate = '';
+    htmlTemplate = '';
+    for (let user of users) {
+        htmlTemplate += `<ul clas="list-group">
+        <li class = "list-group-item"> ID :${user.id}</li>
+        <li class = "list-group-item"> ID :${user.name}</li>
+        <li class = "list-group-item"> ID :${user.email}</li>
+        <li class = "list-group-item"> ID :${user.address.street}</li>
+        <li class = "list-group-item"> ID :${user.address.city}</li>
+        </ul>
+        `
+    }
+    document.querySelector('#api-card').innerHTML = htmlTemplate
 }
 
